9e39bfc63488db00ab80cebfa15d63ed567c63bb
h64 <hhongnr@gmail.com>
2019-10-02 13:03:16 -0700

    file name correction

--- antSandwiches.py
+++ antSandwiches.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+# import numpy as np
 # pandas dataframe - similar to an excel table
 
 # 3 ways of creating a dataframe - One method is manually creating
@@ -7,14 +7,14 @@ import numpy as np
 # Second method is importing a file and converting to DF
 dataset = pd.read_csv('https://vincentarelbundock.github.io/Rdatasets/csv/Stat2Data/SandwichAnts.csv')
 
-sandwich_set = set()
+bread_set = set()
 filling_set = set()
 sandwich_dict = {}
 filling_dict = {}
 
 for row in dataset.values:
-    if row[2] not in sandwich_set:
-        sandwich_set.add(row[2])
+    if row[2] not in bread_set:
+        bread_set.add(row[2])
         sandwich_dict[row[2]] = [row[5]]
     else:
         sandwich_dict[row[2]].append(row[5])
@@ -31,15 +31,16 @@ rows, cols = dataset.shape # df.shape
 # print(dataset.info()) # Show Datatypes
 # print(dataset.describe()) # Show All Stats (count, mean, std, mean, max)
 # print(dataset) # Show whole dataset
+# print(dataset.tail()) # Show last few rows
 
 avg_ants = dataset['Ants'].mean()
 print(f"On average, there are {avg_ants} ants on any given sandwich")
 
-sandwich_data = pd.DataFrame(sandwich_dict) # Convert dictionary to DataFrame
+bread_data = pd.DataFrame(sandwich_dict) # Convert dictionary to DataFrame
 filling_data = pd.DataFrame(filling_dict)
-# print(sandwich_set)
+# print(bread_set)
 # print(filling_set)
-# print(sandwich_data)
+# print(bread_data)
 # print(filling_data)
 
 above_avg = {}
@@ -51,8 +52,8 @@ for key in filling_set:
     else:
         above_avg[key] = num_ants
 
-for key in sandwich_set:
-    num_ants = sandwich_data[key].mean() 
+for key in bread_set:
+    num_ants = bread_data[key].mean() 
     if num_ants < avg_ants:
         below_avg[key] = num_ants 
     else:
@@ -66,10 +67,10 @@ print(f'Above avg: {above_avg}')
 # results.to_filetype('results.csv')
 
 bread_correlations = {
-    'White': sandwich_data['White'].corr(dataset['Ants']),
-    'WholeWheat': sandwich_data['WholeWheat'].corr(dataset['Ants']),
-    'MultiGrain': sandwich_data['MultiGrain'].corr(dataset['Ants']),
-    'Rye': sandwich_data['Rye'].corr(dataset['Ants'])
+    'White': bread_data['White'].corr(dataset['Ants']),
+    'WholeWheat': bread_data['WholeWheat'].corr(dataset['Ants']),
+    'MultiGrain': bread_data['MultiGrain'].corr(dataset['Ants']),
+    'Rye': bread_data['Rye'].corr(dataset['Ants'])
 }
 print(bread_correlations)
 
@@ -81,6 +82,10 @@ filling_correlations = {
 print(filling_correlations)
 
 # print(dataset[dataset.columns[2:]])
-# print(pd.concat([sandwich_data, filling_data], axis=1, keys=["sandwich_data", "filling_data"]).corr())
-print(pd.concat([sandwich_data, filling_data], axis=1, keys=["sandwich_data", "filling_data"]).corr().loc['sandwich_data', 'filling_data'])
+# print(pd.concat([bread_data, filling_data], axis=1, keys=["bread_data", "filling_data"]).corr())
+# print(corr.columns[:][0])
+# Crazy correlation between two dataframes
+# https://stackoverflow.com/questions/41823728/how-to-perform-correlation-between-two-dataframes-with-different-column-names
+corr = pd.concat([bread_data, filling_data], axis=1, keys=["bread_data", "filling_data"]).corr().loc['bread_data', 'filling_data']
+print(corr)
 # print(dataset)
